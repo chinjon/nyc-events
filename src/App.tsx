50a4b91bb4945a8efe0cfbfb9d7d4fdd0560ce51
Jonathan Chin <jonathankchin@gmail.com>
2024-09-19 12:09:24 -0400

    feat: add query util

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, Suspense } from 'react'
 import './App.css'
+import createApiQuery from './core/utils/create-api-query';
+
+const baseApiUrl = "https://data.cityofnewyork.us/resource/tvpp-9vvx.json"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<[]>([]);
+  const requestQuery = createApiQuery({limit: "100"})
+  const reqUrl = encodeURI(`${baseApiUrl}${requestQuery}`);
+
+  useEffect(() => {
+    try {
+      fetch(reqUrl)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+    } catch (error) {
+      throw new Error(`${error}`)
+    }
+  }, []);
+
+  console.log(data)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Suspense>
+          <div className="table-container">
+          </div>
+        </Suspense>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
