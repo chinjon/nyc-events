83550ed5215fa12d42c35867bda75d8c076b7a79
Jonathan Chin <jonathankchin@gmail.com>
2024-09-29 19:12:59 -0400

    feat: add order query

--- src/core/utils/create-api-query.ts
+++ src/core/utils/create-api-query.ts
@@ -3,11 +3,15 @@ const whereQuery = (whereFilter: { prop: string, conditional: string, value: str
   return `&$where=${filterQueries.join(' AND ')}`;
 }
 
+const orderQuery = (column: string, order: string) => {
+  return `$order=${column} ${order}`;
+}
+
 const createApiQuery = (queryObj: { limit?: string, excludeType?: string[] }) => {
   let query = "?";
 
   if (queryObj.limit) {
-    query += `$limit=${queryObj.limit}`;
+    query += `$limit=${queryObj.limit}&${orderQuery('start_date_time', 'DESC')}`;
     query += whereQuery([{ prop: 'event_type', conditional: 'not like', value: 'Sport - Adult' }, { prop: 'event_type', conditional: 'not like', value: 'Sport - Youth' }]);
   }
 
