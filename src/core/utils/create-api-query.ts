f7540b3c450ead941653f1234bfd3914cdb0d6d2
Jonathan Chin <jonathankchin@gmail.com>
2024-09-29 19:01:05 -0400

    feat: add where query generator

--- src/core/utils/create-api-query.ts
+++ src/core/utils/create-api-query.ts
@@ -1,11 +1,19 @@
-const createApiQuery = (queryObj: {limit?: string}) => {
+const whereQuery = (whereFilter: { prop: string, conditional: string, value: string }[]) => {
+  const filterQueries = whereFilter.map((filter) => `${filter.prop} ${filter.conditional} '${filter.value}'`);
+  return `&$where=${filterQueries.join(' AND ')}`;
+}
+
+const createApiQuery = (queryObj: { limit?: string, excludeType?: string[] }) => {
   let query = "?";
-  
+
   if (queryObj.limit) {
     query += `$limit=${queryObj.limit}`;
+    query += whereQuery([{ prop: 'event_type', conditional: 'not like', value: 'Sport - Adult' }, { prop: 'event_type', conditional: 'not like', value: 'Sport - Youth' }]);
   }
 
   return query;
 }
+
+
 
 export default createApiQuery;
