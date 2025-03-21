0ae62e463794abb1aa573c419c33ed128fb7c18d
Jonathan Chin <jonathankchin@gmail.com>
2025-03-21 10:24:05 -0400

    feat: remove search query
    moving towards only using date as query

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,22 +1,15 @@
 import {ChangeEvent, FormEvent, useState} from 'react';
 
-export default function Search({query = ""}: {query: string}) {
-  const [userQuery, setQuery] = useState(query)
-
-  function updateQuery(e: ChangeEvent<HTMLInputElement>) {
-    console.log(e?.target?.value)
-    setQuery(e?.target.value);
-  }
+export default function Search() {
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
-    console.log(userQuery, 'submit')
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input name="searchQueryInput" placeholder="enter search query" onChange={updateQuery} />
+
         <button>Search</button>
       </form>
     </>
