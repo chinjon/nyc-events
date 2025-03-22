df5f91ac35d74d2e43c4211d098980879e7a1ff8
Jonathan Chin <jonathankchin@gmail.com>
2025-03-21 20:52:14 -0400

    feat: update to fixed start and end of year
    simplify search

--- src/core/utils/create-api-query.ts
+++ src/core/utils/create-api-query.ts
@@ -1,3 +1,5 @@
+import { formatISO, startOfYear, endOfYear } from "date-fns";
+
 const whereQuery = (whereFilter: { prop: string, conditional: string, value: string }[]) => {
   const filterQueries = whereFilter.map((filter) => `${filter.prop} ${filter.conditional} '${filter.value}'`);
   return `&$where=${filterQueries.join(' AND ')}`;
@@ -7,29 +9,20 @@ const orderQuery = (column: string, order: string) => {
   return `$order=${column} ${order}`;
 }
 
-const whereDateQuery = (whereDateFilter: {start: string, end: string}) => {
-  // const start = new Date(whereDateFilter.start).toISOString();
-  // const end = new Date(whereDateFilter.end).toISOString();
-
+const whereDateQuery = (whereDateFilter: { start: string, end: string }) => {
   return ` AND start_date_time between '${whereDateFilter.start}' and '${whereDateFilter.end}'`
 }
 
-const createApiQuery = (queryObj: { limit?: string, excludeType?: string[] }) => {
+const createApiQuery = (queryObj: { limit?: string, start?: string, end?: string }) => {
   let query = "?";
 
   if (queryObj.limit) {
-    query += `$limit=${queryObj.limit}&${orderQuery('start_date_time', 'DESC')}`;
+    query += `$limit=${queryObj.limit}&${orderQuery('start_date_time', 'ASC')}`;
     query += whereQuery([{ prop: 'event_type', conditional: 'like', value: 'Parade' }]);
-    query += whereDateQuery({start: "2025-03-29T05:00:01", end: "2025-05-29T23:11:59"});
+    query += whereDateQuery({ start: new Date(formatISO(startOfYear(new Date()))).toISOString().replace('Z', ''), end: new Date(formatISO(endOfYear(new Date()))).toISOString().replace('Z', '') });
   }
-
-  const testDate = whereDateQuery({start: "2025-01-29T05:00:00", end: "2025-01-29T23:11:00"})
-  console.log(testDate)
-  console.log(query)
 
   return query;
 }
-
-
 
 export default createApiQuery;
