54c70e1f319950de1b4024c936fc20c6b32e551b
Jonathan Chin <jonathankchin@gmail.com>
2025-03-22 16:11:53 -0400

    feat: add badge for borough

--- src/components/event-card/event-card.tsx
+++ src/components/event-card/event-card.tsx
@@ -20,6 +20,7 @@ export default function EventCard(eventInfo: { eventInfo: Event }) {
             <div>{dateDay}</div>
           </div>
           <div className="event-date-year">{dateYear}</div>
+          <div><span className="badge">{eventInfo.eventInfo.event_borough}</span></div>
         </div>
         <div>
           <div className="event-title">{eventInfo.eventInfo.event_name}</div>
