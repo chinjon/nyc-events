7021884760f7a0797c8a8237d3adc270e9fe00af
Jonathan Chin <jonathankchin@gmail.com>
2025-03-22 15:52:03 -0400

    doc: add disclaimer in header

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,25 @@ function App() {
           <h1>Upcoming New York City Parades - 2025</h1>
           <p>
             Sourcing data from NYC Opendata, this page should display parades
-            with active permits filed. As per the data set page, permits for the next month will only be part of the data set.
+            with active permits filed. As per the data set page, permits for the
+            next month will only be part of the data set. While not a
+            replacement for the
+            <a href="https://www.nyc.gov/events/index.html" target="_blank">
+              NYC Events page
+            </a>
+            , this is just a straight forward reference if you're looking for a
+            parade.
+          </p>
+          <p>
+            This is data straight from
+            <a
+              href="https://data.cityofnewyork.us/City-Government/NYC-Permitted-Event-Information/tvpp-9vvx/about_data"
+              target="_blank"
+            >
+              Powered by NYC Opendata
+            </a>
+            , I do not control what events show up, though I am open to
+            filtering out some of the monthly processions.
           </p>
         </header>
         <Suspense>
