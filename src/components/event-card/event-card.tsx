9edf8a382c151d34a2e75fb1880c22303d811a9b
Jonathan Chin <jonathankchin@gmail.com>
2025-03-21 23:58:27 -0400

    feat: update event card style

--- src/components/event-card/event-card.tsx
+++ src/components/event-card/event-card.tsx
@@ -5,10 +5,21 @@ export default function EventCard(eventInfo: { eventInfo: Event }) {
   return (
     <>
       <div className="event-card">
-        <div className="event-title">{eventInfo.eventInfo.event_name}</div>
-        <div><span className="event-type">{eventInfo.eventInfo.event_type}</span> <span>{new Intl.DateTimeFormat("en-us").format(new Date(eventInfo.eventInfo.start_date_time as string))}</span></div>
-        <div className="truncate">{eventInfo.eventInfo.event_location}</div>
+        <div>
+          <span>
+            {new Intl.DateTimeFormat("en-us").format(
+              new Date(eventInfo.eventInfo.start_date_time as string)
+            )}
+          </span>
+        </div>
+        <div>
+          <div className="event-title">{eventInfo.eventInfo.event_name}</div>
+          <div>
+            <span className="event-type">{eventInfo.eventInfo.event_type}</span>{" "}
+          </div>
+          <div className="truncate">{eventInfo.eventInfo.event_location?.split(",")[0]}</div>
+        </div>
       </div>
     </>
-  )
+  );
 }
--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -3,7 +3,6 @@ import { Event } from "../../core/types/event.interface";
 import "./events.css";
 
 export default function Events({ eventsData }: { eventsData: Event[] }) {
-  console.log(eventsData)
   const eventCards = eventsData.map((event) => <EventCard key={event.event_id} eventInfo={event}></EventCard>)
   return (
     <>
