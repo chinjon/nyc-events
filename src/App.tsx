2eba08c11129679e40442de13ce1bd588a49bcc4
Jonathan Chin <jonathankchin@gmail.com>
2025-03-22 09:25:48 -0400

    feat: run prettier, update layout
    added text about data on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,62 @@
-import { useState, useEffect, Suspense } from 'react'
-import './App.css'
-import createApiQuery from './core/utils/create-api-query';
-import Events from './components/events/events';
+import { useState, useEffect, Suspense } from "react";
+import "./App.css";
+import createApiQuery from "./core/utils/create-api-query";
+import Events from "./components/events/events";
 
-const baseApiUrl = "https://data.cityofnewyork.us/resource/tvpp-9vvx.json"
+const baseApiUrl = "https://data.cityofnewyork.us/resource/tvpp-9vvx.json";
 
 function App() {
   const [data, setData] = useState<[]>([]);
 
-  const requestQuery = createApiQuery({limit: "100"})
+  const requestQuery = createApiQuery({ limit: "100" });
   const reqUrl = encodeURI(`${baseApiUrl}${requestQuery}`);
 
   useEffect(() => {
     try {
       fetch(reqUrl)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-      });
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+        });
     } catch (error) {
-      throw new Error(`${error}`)
+      throw new Error(`${error}`);
     }
   }, []);
 
   return (
     <>
       <div>
-        <div>
+        <header className="header-content">
           <h1>Upcoming New York City Parades - 2025</h1>
           <p>
-            
+            Sourcing data from NYC Opendata, this page should display parades
+            with active permits filed. It seems that these permits don't
+            "activate" until closer to date of the parade, hence why parades
+            later in the year are not displaying.
           </p>
-        </div>
+        </header>
         <Suspense>
           <div className="table-container">
-            <Events eventsData={data}/>
+            <Events eventsData={data} />
           </div>
         </Suspense>
       </div>
+      <footer className="footer-content">
+        <a
+          href="https://data.cityofnewyork.us/City-Government/NYC-Permitted-Event-Information/tvpp-9vvx/about_data"
+          target="_blank"
+        >
+          Powered by NYC Opendata
+        </a>
+        |
+        <a href="https://github.com/chinjon/nyc-events" target="_blank">
+          Source code
+        </a>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
