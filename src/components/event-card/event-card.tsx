95f3d0c29ed7c35fbbcdf3e019110eca3d79410d
Jonathan Chin <jonathankchin@gmail.com>
2025-03-22 10:19:06 -0400

    feat: update layout and styling of date

--- src/components/event-card/event-card.tsx
+++ src/components/event-card/event-card.tsx
@@ -1,23 +1,34 @@
 import { Event } from "../../core/types/event.interface";
 import "./event-card.css";
+import dayjs from 'dayjs'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+
 
 export default function EventCard(eventInfo: { eventInfo: Event }) {
+  dayjs.extend(localizedFormat);
+  const date = new Date(eventInfo.eventInfo.start_date_time as string);
+  const dateMonth = dayjs(date).format("MMM");
+  const dateDay = dayjs(date).format("DD");
+  const dateYear = dayjs(date).format("YYYY");
+
   return (
     <>
       <div className="event-card">
-        <div>
-          <span>
-            {new Intl.DateTimeFormat("en-us").format(
-              new Date(eventInfo.eventInfo.start_date_time as string)
-            )}
-          </span>
+        <div className="event-date">
+          <div className="event-date-month-day">
+            <div>{dateMonth}</div>
+            <div>{dateDay}</div>
+          </div>
+          <div className="event-date-year">{dateYear}</div>
         </div>
         <div>
           <div className="event-title">{eventInfo.eventInfo.event_name}</div>
           <div>
             <span className="event-type">{eventInfo.eventInfo.event_type}</span>{" "}
           </div>
-          <div className="truncate">{eventInfo.eventInfo.event_location?.split(",")[0]}</div>
+          <div className="truncate">
+            {eventInfo.eventInfo.event_location?.split(",")[0]}
+          </div>
         </div>
       </div>
     </>
