dc1d7d5af41b8b450eaef6901e4f6ab20b50f1f4
Jonathan Chin <jonathankchin@gmail.com>
2025-04-02 14:43:05 -0400

    feat: remove react router dom init

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import App from './App.tsx'
 import './reset.css'
 import './minimal.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root") as Element).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
