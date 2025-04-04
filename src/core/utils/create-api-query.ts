e6e3170817b47d184f0bcdd81c3f406e08c9c646
Jonathan Chin <jonathankchin@gmail.com>
2025-04-04 10:40:49 -0400

    feat: add core files to astro project

---
+++ src/core/utils/create-api-query.ts
@@ -0,0 +1,28 @@
+import { formatISO, startOfYear, endOfYear } from "date-fns";
+
+const whereQuery = (whereFilter: { prop: string, conditional: string, value: string }[]) => {
+  const filterQueries = whereFilter.map((filter) => `${filter.prop} ${filter.conditional} '${filter.value}'`);
+  return `&$where=${filterQueries.join(' AND ')}`;
+}
+
+const orderQuery = (column: string, order: string) => {
+  return `$order=${column} ${order}`;
+}
+
+const whereDateQuery = (whereDateFilter: { start: string, end: string }) => {
+  return ` AND start_date_time between '${whereDateFilter.start}' and '${whereDateFilter.end}'`
+}
+
+const createApiQuery = (queryObj: { limit?: string, start?: string, end?: string }) => {
+  let query = "?";
+
+  if (queryObj.limit) {
+    query += `$limit=${queryObj.limit}&${orderQuery('start_date_time', 'ASC')}`;
+    query += whereQuery([{ prop: 'event_type', conditional: 'like', value: 'Parade' }]);
+    query += whereDateQuery({ start: new Date(formatISO(startOfYear(new Date()))).toISOString().replace('Z', ''), end: new Date(formatISO(endOfYear(new Date()))).toISOString().replace('Z', '') });
+  }
+
+  return query;
+}
+
+export default createApiQuery;
