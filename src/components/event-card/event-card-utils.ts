ee87fd999762aa59e10814764f5d5e57af345143
jonathan chin <jonathan.chin@posteo.net>
2026-02-16 10:46:06 -0500

    feat: update functionality

--- src/components/event-card/event-card-utils.ts
+++ src/components/event-card/event-card-utils.ts
@@ -1,8 +1,18 @@
 export const prerender = true;
-export const boroughClassName = (borough: string) => {
+
+export const boroughBadgeStyle = (borough: string) => {
   const boroughNormalized = borough.toLowerCase();
-  if (boroughNormalized != "staten island") {
-    return boroughNormalized;
+
+  switch(boroughNormalized) {
+    case "staten island":
+      return "bg-blue-50 text-blue-700"
+    case "manhattan":
+      return "bg-red-50 text-red-700"
+    case "bronx":
+      return "bg-green-50 text-green-700"
+    case "queens":
+      return "bg-purple-50 text-purple-700"
+    case "brooklyn":
+      return "bg-orange-50 text-orange-700"
   }
-  return boroughNormalized.split(" ").join("-");
-};
+}
